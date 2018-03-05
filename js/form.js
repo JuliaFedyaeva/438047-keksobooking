666e4fffa47c2a69da692e5e0f383808d608a72f
Julia <julia.fedyaeva@mail.ru>
2018-03-05 09:13:31 +0300

    Удаляет строки

--- js/form.js
+++ js/form.js
@@ -79,13 +79,10 @@
     window.card.popupRemoveHandler();
     window.pin.removeAll();
     window.map.setInactiveState();
-    window.dragndrop.removeAvatar();
-    window.dragndrop.removeHousePhoto();
   }
 
   function succsessSendFormHandler() {
     defaultValueFormHandler();
-
   }
 
   selectNoticeForm.addEventListener('submit', function (evt) {
